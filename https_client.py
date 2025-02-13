6f76d7b421ab46a0982398b037a7aa2a048e0a6b
jean karia <jeanbuenokaria@gmail.com>
2025-02-12 22:49:20 -0300

    verificação de  certificado ativada

--- https_client.py
+++ https_client.py
@@ -1,13 +1,9 @@
 import requests # type: ignore
-import urllib3 # type: ignore
-
-# Desativando aviso de certificado autoassinado (apenas para testes)
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 URL = "https://localhost:4443"
 
 try:
-    response = requests.get(URL, verify=False)  # Não verifica SSL (apenas para testes locais)
+    response = requests.get(URL, verify="certs/myCA.pem")  # Verifica o certificado da CA
     print(f"Status Code: {response.status_code}")
     print(f"Resposta:\n{response.text}")
 except requests.exceptions.RequestException as e:
